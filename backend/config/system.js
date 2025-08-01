f9a77860339555651245c8551d0210de129aae66
MyHuyn0707 <Nttm07072004@gmail.com>
2025-08-01 23:19:24 +0700

    config: Set up system configuration and database connection

---
+++ backend/config/system.js
@@ -0,0 +1,6 @@
+// define prefix
+const ADMIN_PREFIX = "admin";
+
+module.exports = {
+    adminPrefix: ADMIN_PREFIX,
+};
--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,47 @@
 const express = require("express");
+const methodOverride = require("method-override");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const cors = require("cors");
+
+require("dotenv").config();
+
+// Connect database
+const database = require("./config/database");
+database.connect();
+
 const app = express();
+const port = process.env.PORT;
 
-app.get("/", (req, res) => {
-    res.send("Hello World!");
-});
+// Cấu hình cookies & session
+app.use(cookieParser("KWJFKWEIFHW"));
+app.use(
+    session({
+        secret: "KWJFKWEIFHW",
+        resave: false,
+        saveUninitialized: false,
+        cookie: { maxAge: 60000 },
+    })
+);
 
-const port = 3000;
+// Dịch vụ tĩnh (thư mục public)
+app.use(express.static("public"));
+
+// Override phương thức HTTP (PUT, DELETE qua form)
+app.use(methodOverride("_method"));
+
+// Cho phép CORS
+app.use(cors());
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}!`);
+    console.log(`App listening on port ${port}`);
+});
+
+app.get("/", (req, res) => {
+    res.json({ message: "Server hoạt động tốt!" });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
 });
