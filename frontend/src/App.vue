8657b8c0e446ec4fdcc32ff59d5eec5c341bc403
MyHuyn0707 <Nttm07072004@gmail.com>
2025-08-02 23:44:14 +0700

    init frontend structure for project

---
+++ frontend/src/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <!-- <AppHeader /> -->
+        <div id="app">
+            <router-view />
+            <!-- là một thẻ placeholder mà Vue Router sử dụng để render nội dung của các route trong ứng dụng Vue.js  -->
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    components: {},
+};
+</script>
