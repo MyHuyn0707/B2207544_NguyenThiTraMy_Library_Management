9bf5635873e9b03afa3c2805a55df4710556cc3f
MyHuyn0707 <Nttm07072004@gmail.com>
2025-08-03 23:21:39 +0700

    feat: Set up routing modules for admin and client pages

---
+++ frontend/src/routers/admin.route.js
@@ -0,0 +1,71 @@
+const requireAdminAuth = (to, from, next) => {
+    try {
+        const token = document.cookie
+            .split(";")
+            .find((cookie) => /\btoken\b/.test(cookie));
+        if (token) {
+            next();
+        } else {
+            next("/admin/auth/login");
+        }
+    } catch (error) {
+        next("/admin/auth/login");
+    }
+};
+
+const adminRoutes = [
+    {
+        path: "/admin/books",
+        name: "book",
+        component: () => import("@/views/admin/pages/books/Book.vue"),
+        beforeEnter: requireAdminAuth,
+    },
+    {
+        path: "/admin/books/:id",
+        name: "book.edit",
+        component: () => import("@/views/admin/pages/books/BookEdit.vue"),
+        beforeEnter: requireAdminAuth,
+    },
+    {
+        path: "/admin/books/create",
+        name: "book.add",
+        component: () => import("@/views/admin/pages/books/CreateBook.vue"),
+        beforeEnter: requireAdminAuth,
+    },
+
+    {
+        path: "/admin/books/borrow",
+        name: "book.borrow",
+        component: () => import("@/views/admin/pages/books/Borrow.vue"),
+        beforeEnter: requireAdminAuth,
+    },
+
+    {
+        path: "/admin/employee/infor",
+        name: "employee.infor",
+        component: () => import("@/views/admin/pages/infor/Infor.vue"),
+        beforeEnter: requireAdminAuth,
+    },
+    //   {
+    //     path: "/admin/*", // Sửa lại định tuyến này để bắt đầu bằng "/"
+    //     name: "notfound",
+    //     component: () => import("@/views/admin/pages/books/NotFound.vue"),
+    //   },
+    // {
+    //   path: "/:catchAll(.*)", // Bắt tất cả các đường dẫn không hợp lệ
+    //   name: "notfound",
+    //   component: () => import("@/views/admin/pages/books/NotFound.vue"),
+    // },
+    {
+        path: "/admin/auth/register",
+        name: "register",
+        component: () => import("@/views/admin/pages/register/Register.vue"),
+    },
+    {
+        path: "/admin/auth/login",
+        name: "login",
+        component: () => import("@/views/admin/pages/login/Login.vue"),
+    },
+];
+
+export default adminRoutes;
---
+++ frontend/src/routers/client.route.js
@@ -0,0 +1,52 @@
+const requireClientAuth = (to, from, next) => {
+    try {
+        const token = document.cookie
+            .split("; ")
+            .find((row) => row.startsWith("tokenUser"))
+            .split("=")[1];
+        if (token) {
+            next();
+        } else {
+            next("/auth/login");
+        }
+    } catch (error) {
+        next("/auth/login");
+    }
+};
+
+const clientRoutes = [
+    {
+        path: "/books",
+        name: "book-client",
+        component: () => import("@/views/client/pages/books/ClientBook.vue"),
+        beforeEnter: requireClientAuth,
+    },
+
+    {
+        path: "/reader/borrow",
+        name: "borrow-client",
+        component: () => import("@/views/client/pages/books/ClientBorrow.vue"),
+        beforeEnter: requireClientAuth,
+    },
+    {
+        path: "/reader/infor",
+        name: "infor-client",
+        component: () => import("@/views/client/pages/infor/ClientInfor.vue"),
+        beforeEnter: requireClientAuth,
+    },
+
+    {
+        path: "/reader/register",
+        name: "register-client",
+        component: () =>
+            import("@/views/client/pages/register/ClientRegister.vue"),
+    },
+
+    {
+        path: "/auth/login",
+        name: "login-client",
+        component: () => import("@/views/client/pages/login/ClientLogin.vue"),
+    },
+];
+
+export default clientRoutes;
